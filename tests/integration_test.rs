12a0ba0a73c719b8ee489e3b6d4ca4fb0231a486
Gustavo De la Cruz Tovar <gusdelact@gmail.com>
2020-03-26 03:58:37 -0600

    Se prueban nuevos elementos struct y enum

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,9 +1,31 @@
-use cajita::*;
 use cajita::digital::Dalek;
 use cajita::digital::Binario;
 use cajita::digital::VersionDalek::V1;
 use cajita::digital::VersionDalek::V2;
 
+///! en esta prueba de integracion, se agregan nuevos elementos para probar la extension del Crate
+enum TipoPoder {
+  NORMAL,
+  ESPECIAL,
+  INVENCIBLE,
+}
+struct Cyborg<'a> {
+   name : &'a str,
+   nivel_poder : TipoPoder,
+}
+
+impl Binario for Cyborg<'_> {
+  fn george(&self) -> bool {
+     match self.nivel_poder {
+       TipoPoder::NORMAL => false,
+       TipoPoder::ESPECIAL => true,
+       TipoPoder::INVENCIBLE => true,
+     }
+  }
+  fn turing(&self) -> bool {
+   true
+  }
+}
 #[test]
 fn un_dalek() {
   let d00 = Dalek { identificador: 42, estado: false , version: V2 };
@@ -20,4 +42,10 @@ fn una_escena_daleks() {
        ];
        let resultado=cajita::digital::escena(daleks.to_vec());
        assert!(resultado);
+}
+
+#[test]
+fn un_cyborg() {
+  let cyb0 = Cyborg {name : "1138", nivel_poder: TipoPoder::ESPECIAL };
+  assert!(cyb0.george());
 }
